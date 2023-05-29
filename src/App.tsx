e7bd99a1b5e76b9fabe958ca1b5a6dddc7bdc456
DevAntonioRogers <ar919061493@gmail.com>
2023-05-28 21:15:51 -0400

    Check Winner Logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 const App = () => {
   const [board, setBoard] = useState([]);
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
 
   //Handle Play
   const handleClick = (index) => {
@@ -16,6 +17,28 @@ const App = () => {
     setBoard(newBoard);
 
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  //Check for a winner
+  const checkWinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setWinner(board[a]);
+        return;
+      }
+    }
   };
 
   //Render Gameboard
