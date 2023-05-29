0838ba6eb715d457b763077ed4885a4cb082afad
DevAntonioRogers <ar919061493@gmail.com>
2023-05-28 20:51:26 -0400

    Created Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-
+import { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [board, setBoard] = useState([]);
+
+  const renderSquare = (index) => <button className="w-16 h-16 bg-teal-800 m-[1px]">{board[index]}</button>;
+  return (
+    <div>
+      <div>
+        <div className="row-1">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="row-2">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="row-3">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
