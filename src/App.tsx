822d36f64ff4eced0dd13653b742891afa271132
DevAntonioRogers <ar919061493@gmail.com>
2023-05-28 21:10:19 -0400

    Handle Play Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import { useState } from "react";
 
+// const initialBoard = Array(9).fill(null);
+
 const App = () => {
   const [board, setBoard] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  //Handle Play
+  const handleClick = (index) => {
+    if (board[index]) {
+      return;
+    }
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
 
   //Render Gameboard
-  const renderSquare = (index) => <button className="w-16 h-16 bg-teal-800 ml-2 rounded-sm">{board[index]}</button>;
+  const renderSquare = (index) => (
+    <button onClick={() => handleClick(index)} className="w-16 h-16 bg-white m-1 rounded-sm">
+      {board[index]}
+    </button>
+  );
 
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center">
       <div>
-        <div>
+        <div className="flex">
           {renderSquare(0)}
           {renderSquare(1)}
           {renderSquare(2)}
         </div>
-        <div>
+        <div className="flex">
           {renderSquare(3)}
           {renderSquare(4)}
           {renderSquare(5)}
         </div>
-        <div>
+        <div className="flex">
           {renderSquare(6)}
           {renderSquare(7)}
           {renderSquare(8)}
